773185b5ea426b7004bc232fbad937c821bd1be0
Laerositus <33722419+Laerositus@users.noreply.github.com>
2022-11-27 20:26:52 +0100

    Updated models

--- src/models/game.ts
+++ src/models/game.ts
@@ -4,7 +4,7 @@ export class Game{
     constructor(
         public _id : string,
         public name: string,
-        public platforms: Array<Platform>,
+        public platforms: Array<String>,
         public releaseDate: Date,
         public totalRuns: number,
         public playerCount: number,
--- src/models/platform.ts
+++ src/models/platform.ts
@@ -5,6 +5,6 @@ export class Platform {
         public _id: string,
         public name: string,
         public releaseDate: Date,
-        public games: Array<Game>
+        // public games: Array<String>
     ){}    
 }
