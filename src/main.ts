83ae1301befa4f9f09532e4db42f11da548e2d87
Laerositus <33722419+Laerositus@users.noreply.github.com>
2023-04-03 13:47:32 +0200

    add icons

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import store from './store'
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './assets/main.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const localUrl = 'http://localhost:3000'
 const remoteUrl = 'https://speedrun-collection.onrender.com'
@@ -16,7 +17,11 @@ const apiPath = '/api'
 const app = createApp(App)
 
 app.use(router)
+// for( const [key,component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
 app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
 app.use(axios, {
     baseUrl: localUrl + apiPath,
 })
